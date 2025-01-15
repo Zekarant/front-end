d7a0375f99788430e2a4c9f986039e2275819efd
CHOMEL <colin42660@gmail.com>
2025-01-15 21:02:29 +0100

    fix(dev): Correct navbar

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Cyclism</a>
+                <a class="navbar-brand" href="/">Cyclisme</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -15,6 +15,7 @@
                         </li>
                         <li class="nav-item" v-else>
                             <span class="nav-link">Bonjour, {{ authStore.user.name }}</span>
+                            <a class="nav-link" href="/calendar">Calendrier</a>
                             <a class="nav-link" @click="logout">Se déconnecter</a>
                         </li>
                     </ul>
